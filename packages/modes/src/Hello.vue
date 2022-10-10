007e5b814da88ee463d2a8d43e2a889b85452b83
zhangxuemei <ZhangXueMei@windimg.com>
2022-10-10 14:53:13 +0800

    props

---
+++ packages/modes/src/Hello.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export default {} as unknown as
+  <T extends string | number>(props: {msg: T, foo: T}) => any
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  msg: string | number,
+  foo: string | number
+}>()
+console.log(typeof props.foo,'------',typeof props.msg)
+/**
+ *  number ------ number
+    Hello.vue:11 string ------ string x 3
+    Hello.vue:11 string ------ number
+    Hello.vue:11 number ------ string
+ */
+</script>
+
+<template>
+  hello
+</template>
--- packages/modes/src/Main.vue
+++ packages/modes/src/Main.vue
@@ -3,6 +3,12 @@
     <router-link to="/">home</router-link>
     <router-link to="/responsibility">责任链模式</router-link>
   </div>
+  <HelloVue :msg="12" :foo="123" />
+  <HelloVue :msg="str" :foo="str" />
+  <HelloVue msg="12" foo="123" />
+  <HelloVue msg="str" foo="str" />
+  <HelloVue :msg="123" foo="str" />
+  <HelloVue msg="str" :foo="123" />
 </template>
 
 <style lang="scss" scoped>
@@ -11,7 +17,8 @@
 
 <script setup lang="ts">
 import { nextTick, ref } from 'vue'
-
+import HelloVue from '@/Hello.vue'
+const str = ''
 
 
 
